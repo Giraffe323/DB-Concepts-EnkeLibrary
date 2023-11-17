2539bfaaf9e2532dd69a0a180111b00e54b051bb
Giraffe323 <80483344+Giraffe323@users.noreply.github.com>
2023-11-17 10:09:15 -0500

    Update 0.EnkeLibrary_AlterAllTbls_ChangeIncrements.sql
    put in correct order

--- Enke Library 11-12-23/Enke Library/2.AlterTables/0.EnkeLibrary_AlterAllTbls_ChangeIncrements.sql
+++ Enke Library 11-12-23/Enke Library/2.AlterTables/0.EnkeLibrary_AlterAllTbls_ChangeIncrements.sql
@@ -1,15 +1,14 @@
 use enkelibrary;
 
 ALTER TABLE Author auto_increment = 100;
-ALTER TABLE Genre auto_increment = 900;
-ALTER TABLE Publisher auto_increment = 1200;
-ALTER TABLE Branch auto_increment = 500;
-ALTER TABLE Employee auto_increment = 800;
-ALTER TABLE Patron auto_increment = 1100;
-ALTER TABLE Book auto_increment = 200;
-ALTER TABLE Book_Author auto_increment = 300;
-ALTER TABLE Book_Genre auto_increment = 400;
+ALTER TABLE Genre auto_increment = 200;
+ALTER TABLE Publisher auto_increment = 300;
+ALTER TABLE Branch auto_increment = 400;
+ALTER TABLE Employee auto_increment = 500;
+ALTER TABLE Patron auto_increment = 600;
+ALTER TABLE Book auto_increment = 700;
+ALTER TABLE Book_Author auto_increment = 800;
+ALTER TABLE Book_Genre auto_increment = 900;
 ALTER TABLE inventory auto_increment = 1000;
-ALTER TABLE checkinout_header auto_increment = 700;
-ALTER TABLE checkinout_detail auto_increment = 600;
-
+ALTER TABLE checkinout_header auto_increment = 1100;
+ALTER TABLE checkinout_detail auto_increment = 1200;
